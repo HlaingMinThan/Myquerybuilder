ad64ec1e5e7b3e7de83e23285df793c8a431bd3a
HlaingMinThan <hlaingminthan92@gmail.com>
2020-11-10 14:48:42 +0630

    DB::table('users')->where(name, god)->orWhere(location, 'taung gyi')->get()

--- DB.php
+++ DB.php
@@ -84,7 +84,20 @@ class DB
        
         return $this;
     }
+    public function orWhere($col, $operator, $value='')
+    {
+        if (func_num_args()===2) {
+            $sql=" or $col = '$operator'";
+            self::$sql.=$sql;
+        }
+        if (func_num_args()===3) {
+            $sql=" or $col $operator '$value'";
+            self::$sql.=$sql;
+        }
+       
+        return $this;
+    }
 }
 
-$user=DB::table('users')->where("name", "new user")->andWhere("location", "taung gyi")->first();
+$user=DB::table('users')->where("name", "god")->orWhere("location", "taung gyi")->get();
 var_dump($user);
